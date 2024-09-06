a6f5fb925621c93d2d048e40358ec12331c8ff4c
florentinojink <florentinojink@163.com>
2024-09-06 15:39:06 +0800

    并发的处理多个连接

--- chat-server/src/main.rs
+++ chat-server/src/main.rs
@@ -1,5 +1,5 @@
 use futures::{SinkExt, StreamExt};
-use tokio::net::TcpListener;
+use tokio::net::{TcpListener, TcpStream};
 
 use tokio_util::codec::{FramedRead, FramedWrite, LinesCodec};
 
@@ -9,22 +9,27 @@ const HELP_MSG: &str = include_str!("../../help.txt");
 async fn main() -> anyhow::Result<()> {
     let server = TcpListener::bind("127.0.0.1:54321").await?;
     loop {
-        let (mut tcp, _) = server.accept().await?;
-        let (reader, writer) = tcp.split();
-        let mut stream = FramedRead::new(reader, LinesCodec::new());
-        let mut sink = FramedWrite::new(writer, LinesCodec::new());
+        let (tcp, _) = server.accept().await?;
+        tokio::spawn(handle_user(tcp));
+    }
+}
 
-        sink.send(HELP_MSG).await?;
+async fn handle_user(mut tcp: TcpStream) -> anyhow::Result<()> {
+    let (reader, writer) = tcp.split();
+    let mut stream = FramedRead::new(reader, LinesCodec::new());
+    let mut sink = FramedWrite::new(writer, LinesCodec::new());
 
-        while let Some(Ok(mut msg)) = stream.next().await {
-            if msg.starts_with("/help") {
-                sink.send(HELP_MSG).await?;
-            } else if msg.starts_with("/quit") {
-                break;
-            } else {
-                msg.push_str(" ❤️");
-                sink.send(msg).await?;
-            }
+    sink.send(HELP_MSG).await?;
+
+    while let Some(Ok(mut msg)) = stream.next().await {
+        if msg.starts_with("/help") {
+            sink.send(HELP_MSG).await?;
+        } else if msg.starts_with("/quit") {
+            break;
+        } else {
+            msg.push_str(" ❤️");
+            sink.send(msg).await?;
         }
     }
+    Ok(())
 }

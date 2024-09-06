051d60bba7d8feed7d678aa3f3da8cd324215e85
florentinojink <florentinojink@163.com>
2024-09-06 10:38:04 +0800

    串行处理多个连接

--- chat-server/src/main.rs
+++ chat-server/src/main.rs
@@ -6,10 +6,9 @@ use tokio::net::TcpListener;
 #[tokio::main]
 async fn main() -> anyhow::Result<()>{
     let server = TcpListener::bind("127.0.0.1:54321").await?;
-    let (mut tcp,_) = server.accept().await?;
-    let mut buffer = [0u8;16];
-
     loop {
+        let (mut tcp,_) = server.accept().await?;
+        let mut buffer = [0u8;16];
         let n = tcp.read(&mut buffer).await?;
         if n == 0 {
             break;

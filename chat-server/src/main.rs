d136111feff5cd45b85b011df5ba6c494c422553
florentinojink <florentinojink@163.com>
2024-09-06 11:44:12 +0800

    将字节流解析为行

--- chat-server/src/main.rs
+++ chat-server/src/main.rs
@@ -1,24 +1,19 @@
-use tokio::io::AsyncReadExt;
-use tokio::io::AsyncWriteExt;
+use futures::{SinkExt, StreamExt};
 use tokio::net::TcpListener;
+
+use tokio_util::codec::{FramedRead, FramedWrite, LinesCodec};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let server = TcpListener::bind("127.0.0.1:54321").await?;
     loop {
         let (mut tcp, _) = server.accept().await?;
-        let mut buffer = [0u8; 16];
-        loop {
-            let n = tcp.read(&mut buffer).await?;
-            if n == 0 {
-                break;
-            }
-            let mut line = String::from_utf8(buffer[..n].to_vec())?;
-            line.pop();
-            line.pop();
-            line.push_str(" ❤️\n");
-
-            let _ = tcp.write(line.as_bytes()).await?;
+        let (reader, writer) = tcp.split();
+        let mut stream = FramedRead::new(reader, LinesCodec::new());
+        let mut sink = FramedWrite::new(writer, LinesCodec::new());
+        while let Some(Ok(mut msg))  =  stream.next().await {
+            msg.push_str(" ❤️");
+            sink.send(msg).await?;
         }
     }
 }

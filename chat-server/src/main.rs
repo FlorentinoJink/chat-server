bd9f7a624ea06f95c8d12a616c6fd7a0defcee9d
florentinojink <florentinojink@163.com>
2024-09-06 16:49:28 +0800

    用户聊天

--- chat-server/src/main.rs
+++ chat-server/src/main.rs
@@ -1,5 +1,5 @@
 use futures::{SinkExt, StreamExt};
-use tokio::net::{TcpListener, TcpStream};
+use tokio::{net::{TcpListener, TcpStream},sync::broadcast::{self,Sender}};
 
 use tokio_util::codec::{FramedRead, FramedWrite, LinesCodec};
 
@@ -8,28 +8,33 @@ const HELP_MSG: &str = include_str!("../../help.txt");
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let server = TcpListener::bind("127.0.0.1:54321").await?;
+    let (tx, _) = broadcast::channel::<String>(32);
     loop {
         let (tcp, _) = server.accept().await?;
-        tokio::spawn(handle_user(tcp));
+        tokio::spawn(handle_user(tcp, tx.clone()));
     }
 }
 
-async fn handle_user(mut tcp: TcpStream) -> anyhow::Result<()> {
+async fn handle_user(mut tcp: TcpStream, tx: Sender<String>) -> anyhow::Result<()> {
     let (reader, writer) = tcp.split();
     let mut stream = FramedRead::new(reader, LinesCodec::new());
     let mut sink = FramedWrite::new(writer, LinesCodec::new());
 
+    let mut rx = tx.subscribe();
+
     sink.send(HELP_MSG).await?;
 
-    while let Some(Ok(mut msg)) = stream.next().await {
-        if msg.starts_with("/help") {
+    while let Some(Ok(mut user_msg)) = stream.next().await {
+        if user_msg.starts_with("/help") {
             sink.send(HELP_MSG).await?;
-        } else if msg.starts_with("/quit") {
+        } else if user_msg.starts_with("/quit") {
             break;
         } else {
-            msg.push_str(" ❤️");
-            sink.send(msg).await?;
+            user_msg.push_str(" ❤️");
+            tx.send(user_msg)?;
         }
+        let peer_msg = rx.recv().await?;
+        sink.send(peer_msg).await?;
     }
     Ok(())
 }
